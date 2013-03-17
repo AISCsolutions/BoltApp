cb45540d81e018d3a78017f3789a84770813c1c3
Justin Love <git@JustinLove.name>
2013-03-17 11:52:58 -0500

    extract diagram to own file

---
+++ script/diagram.js
@@ -0,0 +1,73 @@
+var Diagram = {
+  clone: function(selector, which) {
+    var my = Object.create(this)
+    my.selector = selector
+    my.which = which
+    return my
+  },
+  ready: function(debugx, debugy) {
+    var $diagram = $(this.selector)
+    var bo = $diagram.offset()
+
+    var image = $diagram.find('img')
+    var so = image.offset()
+
+    this.sx = so.left - bo.left
+    this.sy = so.top - bo.top
+    this.sw = image.prop('width')
+    this.sh = image.prop('height')
+
+    this.placements()
+  },
+  placements: function() {
+    var my = this
+    $(this.selector).find('.measurement').each(function() {
+      my.place($(this))
+    })
+  },
+  x: function(x) {
+    return (x*this.sw) + this.sx
+  },
+  y: function(y) {
+    return (y*this.sh) + this.sy
+  },
+  ux: function(x) {
+    return (x - this.sx) / this.sw
+  },
+  uy: function(y) {
+    return (y - this.sy) / this.sh
+  },
+  place: function($m) {
+    if ($m.length == 0) {
+      return
+    }
+    var mx = parseInt($m.data('x'), 10) / 100.0
+    var my = parseInt($m.data('y'), 10) / 100.0
+
+    this.placeAt($m, this.x(mx), this.y(my))
+  },
+  placeAt: function($m, x, y) {
+    if ($m.length == 0) {
+      return
+    }
+
+    var px = function(n) {
+      return n.toString() + 'px'
+    }
+
+    var mw = $m.width() / 2
+    var mh = $m.height() / 2
+    $m.css('left', px(x - mw))
+    $m.css('top', px(y - mh))
+  },
+  interactivePlace: function(measurement) {
+    var diagram = this
+    $(document).on('mousemove', function(e) {
+      var offset = $('.diagrams li').offset()
+      var mousex = e.pageX - offset.left
+      var mousey = e.pageY - offset.top
+      diagram.placeAt($('[title="'+measurement+'"]'), mousex, mousey)
+      console.log(diagram.ux(mousex), diagram.uy(mousey))
+    })
+  }
+}
