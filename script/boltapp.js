4191b505f3d015ac69c5a5678b0c50fcd7660f42
Justin Love <git@JustinLove.name>
2013-03-17 07:46:23 -0500

    move measurement parsing to own file

--- script/boltapp.js
+++ script/boltapp.js
@@ -38,35 +38,9 @@ $(function() {
     })
   }
 
-  var parseMeasurements = function(table) {
-    var dims = []
-    var kind = ''
-    var property = ''
-
-    table.forEach(function(row) {
-      if (row[0] != '') {
-        kind = row[0]
-      }
-
-      property = row[1]
-
-      row.slice(3).forEach(function(d, i) {
-        if (!dims[i]) {
-          dims[i] = {}
-        }
-        if (!dims[i][kind]) {
-          dims[i][kind] = {}
-        }
-        dims[i][kind][property] = d
-      })
-    })
-
-    return dims
-  }
-
   var setupDimenions = function() {
+    loadMeasurements()
     wireDiameter()
-    loadCsv('data/measurements.csv', function(data) { window.measurements = parseMeasurements(data)})
   }
 
   $('#dimensions').on('pageshow', setupDimenions)
---
+++ script/measurements.js
@@ -0,0 +1,31 @@
+var loadMeasurements = function() {
+  loadCsv('data/measurements.csv', function(data) {
+    window.measurements = parseMeasurements(data)
+  })
+}
+
+var parseMeasurements = function(table) {
+  var dims = []
+  var kind = ''
+  var property = ''
+
+  table.forEach(function(row) {
+    if (row[0] != '') {
+      kind = row[0]
+    }
+
+    property = row[1]
+
+    row.slice(3).forEach(function(d, i) {
+      if (!dims[i]) {
+        dims[i] = {}
+      }
+      if (!dims[i][kind]) {
+        dims[i][kind] = {}
+      }
+      dims[i][kind][property] = d
+    })
+  })
+
+  return dims
+}
