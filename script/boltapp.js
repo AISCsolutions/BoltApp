7e67fd95bee48451bd56ea1eb2718caf6c991cbf
Justin Love <git@JustinLove.name>
2013-03-20 16:56:36 -0500

    fix NW layout again

--- script/boltapp.js
+++ script/boltapp.js
@@ -151,13 +151,14 @@ define([
 
   /* Nuts and Washers */
   var fixNW = function() {
-    //console.log('fixing NW')
     var headerHeight = $.mobile.activePage.find('[data-role="header"]').height()
     var footerHeight = $.mobile.activePage.find('[data-role="footer"]').height()
     var windowHeight = $(this).height()
     var contentPadding = parseInt($.mobile.activePage.find('[data-role="content"]').css("padding-top"), 10)
     var pagePadding = parseInt($.mobile.activePage.css("padding-top"), 10)
-    $('#nuts-and-washers .split').css('height', windowHeight - headerHeight - footerHeight - pagePadding - contentPadding*2)
+    var height = windowHeight - headerHeight - footerHeight - pagePadding - contentPadding*2
+    console.log('fixNW', height)
+    $('#nuts-and-washers .split').css('height', height)
   }
 
   /* Grade Select */
@@ -205,7 +206,7 @@ define([
       setupBoltId()
       $('#bolt-id').on('pagebeforeshow', setupBoltId)
       $('#dimensions').on('pageshow', setupDimensions)
-      $(window).on('navigate', fixNW)
+      $('#nuts-and-washers').on('pagebeforeshow', fixNW)
       $('#grade').on('pagebeforeshow', wireGrade)
       $('#finish').on('pagebeforeshow', wireFinish)
       $('#manufacturer').on('pagebeforeshow', wireManufacturer)
