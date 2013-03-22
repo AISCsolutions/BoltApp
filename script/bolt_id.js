03e95fcd256e33ed46d9e4d983c080614dc3b008
Justin Love <git@JustinLove.name>
2013-03-21 21:41:20 -0500

    refactoring bolt id

--- script/bolt_id.js
+++ script/bolt_id.js
@@ -1,4 +1,9 @@
 define(['jquery', 'appstate'], function($, appstate) {
+  var setupGrade = function() {
+    $('.current-grade .ui-btn-text').html(appstate.data.bolt.grade)
+    $('.ui-btn-text .current-grade').html(appstate.data.bolt.grade)
+  }
+
   var setGlobalClasses = function() {
     $body = $('body')
     if (appstate.data.bolt.type == '3') {
@@ -8,15 +13,20 @@ define(['jquery', 'appstate'], function($, appstate) {
     }
   }
 
-  var setupBoltId = function() {
-    $('.current-grade .ui-btn-text').html(appstate.data.bolt.grade)
-    $('.ui-btn-text .current-grade').html(appstate.data.bolt.grade)
-    $('#finish-select .ui-btn-text').html(appstate.data.bolt.finish)
-    setupBoltIdManufacturer()
-    setupBoltIdType()
+  var setupType = function() {
+    $('label[for="type-'+appstate.data.bolt.type+'"]').click()
+    $('.type input[type="radio"]').on('change', function() {
+      appstate.data.bolt.type = $(this).val()
+      appstate.save()
+      setGlobalClasses()
+    })
   }
 
-  var setupBoltIdManufacturer = function() {
+  var setupFinish = function() {
+    $('#finish-select .ui-btn-text').html(appstate.data.bolt.finish)
+  }
+
+  var setupManufacturer = function() {
     mfg = appstate.data.bolt.manufacturer
     $mfg = $('#manufacturer-select')
     $mfg.find('.name').html(mfg.name)
@@ -25,13 +35,11 @@ define(['jquery', 'appstate'], function($, appstate) {
     $mfg.find('.bolt').attr('src', mfg.bolt)
   }
 
-  var setupBoltIdType = function() {
-    $('label[for="type-'+appstate.data.bolt.type+'"]').click()
-    $('.type input[type="radio"]').on('change', function() {
-      appstate.data.bolt.type = $(this).val()
-      appstate.save()
-      setGlobalClasses()
-    })
+  var setupBoltId = function() {
+    setupGrade()
+    setupType()
+    setupFinish()
+    setupManufacturer()
   }
 
   return {
