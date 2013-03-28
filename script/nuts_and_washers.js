9219a1ed2e5d033663e3cd13c2a43a99e59a8c16
Justin Love <git@JustinLove.name>
2013-03-28 10:52:47 -0500

    washer image, show none while illegal

--- script/nuts_and_washers.js
+++ script/nuts_and_washers.js
@@ -23,7 +23,11 @@ define(['jquery'], function($) {
     },
     wire: function() {
       var nw = this.rules.gradeTypeFinish()
-      if (nw) { this.update(nw) }
+      if (nw) {
+        this.update(nw)
+      } else {
+        this.update({})
+      }
       this.$el().on('click', 'li a', function() {
         $('.zoom').click(function() {$('.ui-dialog').dialog('close')})
         $(this).find('[title]').each(function() {
@@ -35,18 +39,31 @@ define(['jquery'], function($) {
     },
     update: function(data) {
       this.updateFields(data)
-      this.updateLegal(data)
+      this.updateLegalNuts(data)
+      this.updateLegalWashers(data)
     },
     updateFields: function(data) {
       for (var name in data) {
         this.$datum(name).html(data[name] || '')
       }
     },
-    updateLegal: function(data) {
-      var nuts = data['Nut Grade'].split(', ')
+    updateLegalNuts: function(data) {
+      var nuts = (data['Nut Grade'] || '').split(', ')
       this.$('li.nut').each(function() {
         var $el = $(this)
         if (nuts.indexOf($el.find('.nut.grade').html()) >= 0) {
+          $el.removeClass('illegal')
+        } else {
+          $el.addClass('illegal')
+        }
+      })
+      return this
+    },
+    updateLegalWashers: function(data) {
+      var type = data['Washer Type']
+      this.$('li.washer').each(function() {
+        var $el = $(this)
+        if ($el.find('.washer.type').html() == type) {
           $el.removeClass('illegal')
         } else {
           $el.addClass('illegal')
