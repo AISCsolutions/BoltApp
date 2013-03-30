67c7f9d4c7973dabb57033ba92531d790a0a0153
Justin Love <git@JustinLove.name>
2013-03-30 16:21:25 -0500

    custom jquery mobile

--- script/bootstrap.js
+++ script/bootstrap.js
@@ -1,4 +1,4 @@
-require.config({paths: {jquery: 'lib/jquery'}})
+require.config({paths: {jquery: 'lib/jquery', jquerymobile: 'lib/jquery.mobile.custom'}})
 require(['boltapp', 'jquery', 'lib/jquery.mobile-1.3.0', 'lib/es5-shim'], function(BoltApp, $) {
   "use strict"
 
