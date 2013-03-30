3905ee339f8c22c130bb8a53cd139c75a96b6bb3
Justin Love <git@JustinLove.name>
2013-03-30 14:01:07 -0500

    image min

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,13 +33,37 @@ module.exports = function(grunt) {
           '../built/index.html': './index.html',
         }
       }
+    },
+    clean: {
+      options: {
+        force: true
+      },
+      images: ['../built/images'],
+    },
+    imagemin: {
+      target: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [
+          {
+            expand: true,
+            src: ['images/**/*'],
+            filter: 'isFile',
+            dest: '../built/'
+          }
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-cssjoin')
   grunt.loadNpmTasks('grunt-css')
   grunt.loadNpmTasks('grunt-contrib-htmlmin')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-imagemin')
 
   grunt.registerTask('default', ['csslint'])
   grunt.registerTask('build', ['cssjoin', 'cssmin', 'htmlmin'])
+  grunt.registerTask('rebuild-images', ['clean:images', 'imagemin'])
 };
