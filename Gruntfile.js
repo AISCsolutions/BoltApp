35befb307eefc7644fc467eb758215c9a4de854f
Justin Love <git@JustinLove.name>
2013-03-30 19:05:32 -0500

    csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function(grunt) {
         src: "style/boltapp.css",
         rules: {
           "import": false,
+          "adjoining-classes": false,
         }
       }
     },
